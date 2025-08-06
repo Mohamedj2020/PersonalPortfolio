a95a2088461ffdd81e51bb7dd60356d1832db175
Mohamedj2020 <Mohamedjirac2020@gmail.com>
2025-08-06 00:06:18 -0400

    Convert Footer component to TypeScript
    Renamed Footer.jsx to Footer.tsx and updated the component to wrap the copyright text in a <small> tag.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Footer = () => (
   <footer style={{ textAlign: "center", padding: "1rem 0" }}>
-    Copyright © 2025 Mohamed Jirac. All Rights Reserved.
+    <small>
+      Copyright © 2025 Mohamed Jirac. All Rights Reserved.
+    </small>
   </footer>
 );
 
