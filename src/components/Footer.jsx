83b09be0c5baba7c2848b8cec559cadf56da7b2f
Mohamedj2020 <Mohamedjirac2020@gmail.com>
2025-08-06 00:04:11 -0400

    Add Footer component
    Introduces a new Footer component displaying copyright information, styled with centered text and padding.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer style={{ textAlign: "center", padding: "1rem 0" }}>
+    Copyright © 2025 Mohamed Jirac. All Rights Reserved.
+  </footer>
+);
+
+export default Footer;
